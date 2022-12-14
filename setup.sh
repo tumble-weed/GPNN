1f376608be28e9428815231cb39e7e9457927287
tmuble-weed <aniketsinghresearch@gmail.com>
2022-12-14 13:03:23 +0000

    for compatible faiss, ffmpeg

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-sudo apt-get install libomp-dev
+sudo apt-get install libomp-dev -y
 pip install -r requirements.txt
 pip install faiss-gpu --no-cache
+
+# conda install -c conda-forge faiss-gpu -y
+# conda install -c pytorch faiss-gpu cudatoolkit=11.6
+conda install -c pytorch faiss-gpu=1.7.3 cudatoolkit=11.3 -y
