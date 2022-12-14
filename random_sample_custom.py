969fa1c5c4de58bf456fb300fd4ffd69124ffb43
tmuble-weed <aniketsinghresearch@gmail.com>
2022-12-14 13:02:32 +0000

    gradcam

--- random_sample_custom.py
+++ random_sample_custom.py
@@ -9,8 +9,8 @@ import skimage.io
 from matplotlib import pyplot as plt
 from model.utils import Timer
 # original_imname = 'images/ILSVRC2012_val_00000013.JPEG'
-# original_imname = 'database/balloons.png'
-original_imname = 'database/volacano.png'
+original_imname = 'database/balloons.png'
+# original_imname = 'database/volacano.png'
 
 output_imname = os.path.join('output',os.path.basename(original_imname))
 output_imname_root,ext = output_imname.split('.')
@@ -22,17 +22,20 @@ from model.my_gpnn import gpnn
 # from model.gpnn import gpnn
 config = {
     'out_dir':'output',
-    'iters':10,
-    'coarse_dim':14,
+    # 'iters':10,
+    'iters':1,#10
+    # 'coarse_dim':14,#
+    'coarse_dim':100,#
     'out_size':0,
     'patch_size':7,
     'stride':1,
     'pyramid_ratio':4/3,
     'faiss':True,
+    # 'faiss':False,
     'no_cuda':False,
     #---------------------------------------------
     'in':None,
-    'sigma':0.75,
+    'sigma':1*0.75,
     'alpha':0.005,
     'task':'random_sample',
     #---------------------------------------------
@@ -43,8 +46,9 @@ config = {
 
 model = gpnn(config)
 with Timer('model run'):
-    output_im = model.run(to_save=True)
-    
+    augmentations = model.run(to_save=True)
+import gradcam
+cams = gradcam.gradcam(augmentations.permute(0,3,1,2),target=1)
 
 # output_im = skimage.io.imread(output_imname)
 
